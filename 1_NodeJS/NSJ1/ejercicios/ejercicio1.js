63ebdab7d733d6a0f5e5060089d280b5c0bfa216
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2026-04-15 20:26:04 -0300

    Update ejercicio1.js
    Added a simple HTTP server that responds with a message.

--- 1_NodeJS/NSJ1/ejercicios/ejercicio1.js
+++ 1_NodeJS/NSJ1/ejercicios/ejercicio1.js
@@ -1,2 +1,13 @@
+import { createServer } from 'node:http';
+
 console.log("Hola mundo desde Node.js");
 console.log("Fin");
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hola mundo desde Node.js\nFin');
+});
+
+server.listen(3001, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3001');
+});
