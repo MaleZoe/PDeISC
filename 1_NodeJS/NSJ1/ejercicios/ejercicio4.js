c607d6f452defcf5776c0d7ca2e3ed49844c1f07
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2026-04-15 20:28:50 -0300

    Update ejercicio4.js

--- 1_NodeJS/NSJ1/ejercicios/ejercicio4.js
+++ 1_NodeJS/NSJ1/ejercicios/ejercicio4.js
@@ -1,6 +1,21 @@
-import { suma, resta, multiplicacion, division } from './calculos.js';
+import { createServer } from 'node:http';
+import { sumar, restar, multiplicar, division } from './calculos.js';
 
-console.log("Suma:", suma(5, 3));
-console.log("Resta:", resta(8, 6));
-console.log("Multiplicación:", multiplicacion(3, 11));
-console.log("División:", division(30, 5));
+console.log(`La suma de 5 + 3 = ${sumar(5, 3)}`);
+console.log(`La resta de 8 - 6 = ${restar(8, 6)}`);
+console.log(`La multiplicacion de 3 * 11 = ${multiplicar(3, 11)}`);
+console.log(`La division de 30 / 5 = ${division(30, 5)}`);
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(
+`La suma de 5 + 3 = ${sumar(5, 3)}
+La resta de 8 - 6 = ${restar(8, 6)}
+La multiplicacion de 3 * 11 = ${multiplicar(3, 11)}
+La division de 30 / 5 = ${division(30, 5)}`
+  );
+});
+
+server.listen(3004, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3004');
+});
