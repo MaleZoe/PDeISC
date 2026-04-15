a8b7652b5baf2008dd8208626dd681afc6e87710
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2026-04-15 20:27:57 -0300

    Update ejercicio3.js
    Updated arithmetic functions and added HTTP server to display results.

--- 1_NodeJS/NSJ1/ejercicios/ejercicio3.js
+++ 1_NodeJS/NSJ1/ejercicios/ejercicio3.js
@@ -1,12 +1,14 @@
-function suma(a, b) {
+import { createServer } from 'node:http';
+
+function sumar(a, b) {
   return a + b;
 }
 
-function resta(a, b) {
+function restar(a, b) {
   return a - b;
 }
 
-function multiplicacion(a, b) {
+function multiplicar(a, b) {
   return a * b;
 }
 
@@ -14,7 +16,21 @@ function division(a, b) {
   return a / b;
 }
 
-console.log("Suma:", suma(4, 5));
-console.log("Resta:", resta(3, 6));
-console.log("Multiplicación:", multiplicacion(2, 7));
-console.log("División:", division(20, 4));
+console.log(`La suma de 4 + 5 = ${sumar(4, 5)}`);
+console.log(`La resta de 3 - 6 = ${restar(3, 6)}`);
+console.log(`La multiplicacion de 2 * 7 = ${multiplicar(2, 7)}`);
+console.log(`La division de 20 / 4 = ${division(20, 4)}`);
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(
+`La suma de 4 + 5 = ${sumar(4, 5)}
+La resta de 3 - 6 = ${restar(3, 6)}
+La multiplicacion de 2 * 7 = ${multiplicar(2, 7)}
+La division de 20 / 4 = ${division(20, 4)}`
+  );
+});
+
+server.listen(3003, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3003');
+});
