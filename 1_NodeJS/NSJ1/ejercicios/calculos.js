29777d65377308540b862e2a55e2d0799e8750b7
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2026-04-15 20:25:01 -0300

    Update calculos.js

--- 1_NodeJS/NSJ1/ejercicios/calculos.js
+++ 1_NodeJS/NSJ1/ejercicios/calculos.js
@@ -1,4 +1,15 @@
-export const sumar = (a, b) => a + b;
-export const restar = (a, b) => a - b;
-export const multiplicar = (a, b) => a * b;
-export const dividir = (a, b) => a / b; 
+export function sumar(a, b) {
+  return a + b;
+}
+
+export function restar(a, b) {
+  return a - b;
+}
+
+export function multiplicar(a, b) {
+  return a * b;
+}
+
+export function division(a, b) {
+  return a / b;
+}
