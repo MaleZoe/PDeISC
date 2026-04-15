f3be3c5022fd78631310f644f2b9f41692cc65ae
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2026-04-15 20:26:45 -0300

    Update ejercicio2.js

--- 1_NodeJS/NSJ1/ejercicios/ejercicio2.js
+++ 1_NodeJS/NSJ1/ejercicios/ejercicio2.js
@@ -1,4 +1,25 @@
-console.log("Suma:", 4 + 5);
-console.log("Resta:", 3 - 6);
-console.log("Multiplicación:", 2 * 7);
-console.log("División:", 20 / 4);
+import { createServer } from 'node:http';
+
+const suma = 4 + 5;
+const resta = 3 - 6;
+const multiplicacion = 2 * 7;
+const division = 20 / 4;
+
+console.log(`La suma de 4 + 5 = ${suma}`);
+console.log(`La resta de 3 - 6 = ${resta}`);
+console.log(`La multiplicacion de 2 * 7 = ${multiplicacion}`);
+console.log(`La division de 20 / 4 = ${division}`);
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(
+`La suma de 4 + 5 = ${suma}
+La resta de 3 - 6 = ${resta}
+La multiplicacion de 2 * 7 = ${multiplicacion}
+La division de 20 / 4 = ${division}`
+  );
+});
+
+server.listen(3002, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3002');
+});
